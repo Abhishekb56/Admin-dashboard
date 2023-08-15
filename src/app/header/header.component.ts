70bc2969b685019373ee57a00734408e1f619682
Abhishekb56 <abhishekpm.899@gmail.com>
2023-08-15 22:18:11 +0530

    dashboard

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+  // output decreator @output decreator
+
+   @Output() onMenuBtnClick=new EventEmitter
+
+
+  menubtnClicked(){
+
+    this.onMenuBtnClick.emit()
+
+
+  }
+
+}
